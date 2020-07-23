578958a188811f7c7eb2fc4c64260eb94401cb9a
Keneth Ade <kenjiwebdev@gmail.com>
2020-07-23 11:16:33 +0800

    Updated portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,12 +18,10 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: `I'm a Self-Taught Developer from the Philippines. 
-  I have a Bachelor degree in Philosophy, and after graduating I decided to fully commit myself to Web Development. `,
-  paragraphTwo: `I have always been passionate about Technology. I love learning, and I am always ready to improve and work with others.`,
-  paragraphThree: `My background has given me strong problem solving skills and good critical thinking, 
-  in both technical and non-technical matters.`,
-  resume: '', // if no resume, the button will not show up
+  paragraphOne: `I'm a Self-Taught Developer from the Philippines.`,
+  paragraphTwo: `I have always been passionate about Technology. I love learning, and I am always ready to improve.`,
+  paragraphThree: `My background on Philosophy and Web Development has given me strong problem solving skills.`,
+  resume: '', // if no resume, the button will not show up  
 };
 
 // PROJECTS DATA
@@ -31,17 +29,17 @@ export const projectsData = [
   {
     id: uuidv1(),
     img: 'project1.png',
-    title: 'Robo Friends React',
-    info: 'A robofriends React.js app',
+    title: 'Crown Clothing Store',
+    info: 'E-Commerce Web App powred by React.js, Redux and Firebase',
     info2: '',
-    url: 'https://kenjiwebdev.github.io/robofriends/',
-    repo: 'https://github.com/kenjiwebdev/robofriends', // if no repo, the button will not show up
+    url: 'https://crown-react-clothing-live.herokuapp.com/',
+    repo: 'https://github.com/kenjiwebdev/crwn-clothing', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
     img: 'project2.png',
     title: 'Face Recognition Brain',
-    info: 'Face Recognition React App',
+    info: 'Image Face Recognition React App',
     info2: '',
     url: 'https://kenjiwebdev.github.io/face-recognition-brain/',
     repo: 'https://github.com/kenjiwebdev/face-recognition-brain', // if no repo, the button will not show up
